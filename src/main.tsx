075acf377a87bae557096f3439a8d4eeddff264e
ruziev-dev <ruziev.dev@gmail.com>
2023-10-17 10:27:42 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import WebApp from '@twa-dev/sdk'
+import eruda from 'eruda'
+
+import './index.css'
+
+WebApp.ready()
+eruda.init()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
