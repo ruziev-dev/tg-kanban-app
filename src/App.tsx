47c17bfe7906c9f0a0e7642399b4dd1496ccb63d
ruziev-dev <ruziev.dev@gmail.com>
2023-10-17 15:11:59 +0300

    Add: parsing initData

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,33 @@ import WebApp from '@twa-dev/sdk'
 const App = () => {
 	console.log(WebApp)
 
+	const initParams = new URLSearchParams(WebApp.initData)
+
+	const user = initParams.get('user')
+	const query_id = initParams.get('query_id')
+	console.log({ query_id })
+
+	console.log(JSON.parse(user || ''))
+
+	/* 	console.log([...initParams.values()])
+	console.log([...initParams.keys()]) */
 	return (
-		<>
-			<div>TEST APP</div>
-		</>
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				width: '100vw',
+				height: '100vh',
+			}}>
+			<div style={{ textAlign: 'center' }}>
+				<h3>Init data</h3>
+				<p>user: {user}</p>
+				<button onClick={() => WebApp.sendData('Send some Data')}>
+					Push it to send some data
+				</button>
+			</div>
+		</div>
 	)
 }
 
